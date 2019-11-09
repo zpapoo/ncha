423fd908f370b88b9deef740a3de1eeb0cdc7f11
JaeYeopHan <ljyhanll@gmail.com>
2019-11-09 18:49:39 +0900

    feat: Customize document

---
+++ pages/_document.tsx
@@ -0,0 +1,30 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+import React from 'react'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang="ko">
+        <Head>
+          <meta charSet="UTF-8" />
+          <link
+            href="https://fonts.googleapis.com/css?family=Noto+Sans+KR:400,700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
