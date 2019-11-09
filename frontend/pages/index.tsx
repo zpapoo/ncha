639f52bd65914ceeac6e5e0ef2566cbe6c052fb3
JaeYeopHan <ljyhanll@gmail.com>
2019-11-09 14:51:26 +0900

    env: Setup next.js development

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home = () => (
+  <>
+    <Head>
+      <title>Home</title>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    </Head>
+    <div>nCha</div>
+  </>
+)
+
+export default Home
