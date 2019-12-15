0902b585f0ef49cd39887e69fbdf2433dede9844
soso <ethdud1@gmail.com>
2019-12-15 18:59:22 +0900

    Disunite mockData and Remove useless style code

--- components/player/index.tsx
+++ components/player/index.tsx
@@ -1,7 +1,10 @@
 import styled from '@emotion/styled'
 import { useRouter } from 'next/router'
 import React from 'react'
+import { useDispatch } from 'react-redux'
 
+import { playerActions } from '../../features/player'
+import { mockData } from '../../mockData/comment'
 import { PlayerTimeLine } from './PlayerTimeLine'
 import { SpeechBubble } from './SpeechBubble'
 
@@ -37,33 +40,6 @@ const PlayerTitle = styled.h2`
   color: #ffffff;
 `
 
-const TimeLineContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 12px 0;
-`
-
-const FormattedTime = styled.span`
-  font-style: normal;
-  font-weight: 500;
-  font-size: 10px;
-  line-height: 14px;
-  display: flex;
-  text-align: center;
-
-  color: #ffffff;
-`
-
-const TimeLine = styled.span`
-  display: flex;
-  margin: 0 12px;
-
-  width: 200px;
-  height: 2px;
-  background-color: #e0115f;
-`
-
 const PlayerButton = styled.div`
   margin: auto;
   width: 0px;
@@ -75,27 +51,7 @@ const PlayerButton = styled.div`
 
 export const Player: React.FC<Props> = () => {
   const { query } = useRouter()
-
-  const mockData = [
-    {
-      data: {
-        name: '소소감독',
-        comments: [
-          '방황하였으며, 우리의 얼마나 심장은 불어 청춘의 이상의 투명하되 것이다. 가슴이 따뜻한 작고 힘있다. 얼음이 무엇을 천고에 쓸쓸하랴? 같으며,설레는 거친 새 장식하는 희망의 얼음과 것 같으며, 설레는 거친 새장식하는 희망의 얼음과 것 같으며, 설레는 거친 새 장식하는 희망의',
-          '가슴이 따뜻한 작고 힘있다.'
-        ]
-      }
-    },
-    {
-      data: {
-        name: '음향감독',
-        comments: [
-          '방황하였으며, 우리의 얼마나 심장은 불어 청춘의 이상의 투명하되 것이다. 가슴이 따뜻한 작고 힘있다. 얼음이 무엇을 천고에 쓸쓸하랴? 같으며,설레는 거친 새 장식하는 희망의 얼음과 것 같으며, 설레는 거친 새장식하는 희망의 얼음과 것 같으며, 설레는 거친 새 장식하는 희망의',
-          '가슴이 따뜻한 작고 힘있다.'
-        ]
-      }
-    }
-  ]
+  const dispatch = useDispatch()
 
   return (
     <>
@@ -103,7 +59,9 @@ export const Player: React.FC<Props> = () => {
         <PlayerTitle>The Dark Night</PlayerTitle>
         <CloseButton />
         <PlayerTimeLine />
-        <PlayerButton />
+        <PlayerButton onClick={ () =>
+          dispatch(playerActions.play())
+        }/>
       </PlayerTop>
       <SpeechBubble data={mockData[0].data}/>
       <SpeechBubble data={mockData[1].data}/>
