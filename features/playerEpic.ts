0e4a702167ad12b42344568f536beae4dddeea7c
soso <ethdud1@gmail.com>
2019-12-15 19:55:22 +0900

    Add interval logic at epic middleware

--- features/playerEpic.ts
+++ features/playerEpic.ts
@@ -1,18 +1,29 @@
 import { PayloadAction } from '@reduxjs/toolkit'
-import { ActionsObservable, Epic, ofType } from 'redux-observable'
-import { tap } from 'rxjs/operators'
+import { ActionsObservable, Epic, ofType, StateObservable } from 'redux-observable'
+import { timer } from 'rxjs'
+import { flatMap, map, takeUntil } from 'rxjs/operators'
 
-import { playerActionName } from './player'
+import { RootState } from '.'
+import { playerActionName, playerActions } from './player'
 
 export const playerEpic: Epic = (
-  action: ActionsObservable<PayloadAction<any>>
+  action$: ActionsObservable<PayloadAction<any>>,
+  store$: StateObservable<RootState>
 ) =>  {
-  console.log('playerEpic')
+  // Timer's first parameter === runningTime
 
   return (
-    action.pipe(
-      ofType(playerActionName),
-      tap(() => console.log(playerActionName))
+    action$.pipe(
+      ofType(`${playerActionName}/play`),
+      flatMap(() => timer(0, 1000)),
+      map((count) => {
+        const currentTime = store$.value.player.currentTime
+
+        return playerActions.onTimeUpdate(currentTime + 1)
+      }),
+      takeUntil(action$.pipe(
+        ofType(`${playerActionName}/pause`)
+      ))
     )
   )
 }
