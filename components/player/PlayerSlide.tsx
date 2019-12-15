332ba49f850e24521e066feb0f4a9a99a3dfde65
soso <ethdud1@gmail.com>
2019-12-15 22:44:08 +0900

    Fix code style

--- components/player/PlayerSlide.tsx
+++ components/player/PlayerSlide.tsx
@@ -5,7 +5,7 @@ interface Props {
   width: number;
 }
 
-const  Slide = styled<'div', Props>('div')`
+const Slide = styled<'div', Props>('div')`
   background: linear-gradient(to right, #EF4D88 0%, #4E51FF 100%);
   height: 100%;
   width: ${(props) => `${props.width}%`};
