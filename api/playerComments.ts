734dcca4226ad6f11908631213ba9ccdc682a843
SoYoung210 <ethdud1@gmail.com>
2020-01-10 16:44:32 +0900

    Fix package manager

---
+++ api/playerComments.ts
@@ -0,0 +1,13 @@
+import { AxiosResponse } from 'axios'
+import { from, Observable } from 'rxjs'
+
+import { playerCommentsMockData } from '__tests__/mockData/comment'
+
+const mockData: Promise<any> = new Promise(resolve =>
+  resolve(playerCommentsMockData),
+)
+
+export const getComments = (id: string): Observable<AxiosResponse> => {
+  // return requestGET(`/api/movie/${id}`)
+  return from(mockData)
+}
