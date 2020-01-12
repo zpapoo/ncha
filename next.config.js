55b4c0da6dca020ed49b2ca603d50eacd9eaefcb
soso <ethdud1@gmail.com>
2020-01-12 18:04:22 +0900

    Add utils alias at next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 module.exports = {
   webpack(config, options) {
     config.resolve.alias['api'] = path.join(__dirname, 'api')
+    config.resolve.alias['utils'] = path.join(__dirname, 'utils')
     config.resolve.alias['components'] = path.join(__dirname, 'components')
     config.resolve.alias['features'] = path.join(__dirname, 'features')
     config.resolve.alias['__tests__'] = path.join(__dirname, '__tests__')
