a0387a6def6b68ccbf37baa9f81e48c85683bac4
SoYoung210 <ethdud1@gmail.com>
2020-01-14 20:16:45 +0900

    Update formatTime util
    - mm:ss to hh:mm:ss

--- __tests__/utils/time.test.ts
+++ __tests__/utils/time.test.ts
@@ -3,27 +3,27 @@ import { formatTime } from 'utils/time'
 describe('[Utils] formatTime', () => {
   it('formatTime - less than 1 minutes', () => {
     // Given
-    const expected = '00:59'
+    const expected = '00:00:59'
     // When
     const result = formatTime(59)
     // Then
     expect(result).toEqual(expected)
   })
 
-  it('formatTime - 59s < seconds < 99m', () => {
+  it('formatTime - 59s < seconds < 1h', () => {
     // Given
-    const expected = '10:59'
+    const expected = '00:10:59'
     // When
     const result = formatTime(659)
     // Then
     expect(result).toEqual(expected)
   })
 
-  it('formatTime - 99m =< seconds', () => {
+  it('formatTime - 1h =< seconds', () => {
     // Given
-    const expected = '100:00'
+    const expected = '33:12:00'
     // When
-    const result = formatTime(6000)
+    const result = formatTime(119520)
     // Then
     expect(result).toEqual(expected)
   })
