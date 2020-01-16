2b3fedfacf478016c852daa8a2318176ca458faa
SoYoung210 <ethdud1@gmail.com>
2020-01-16 13:36:45 +0900

    Fix jest moduleNameMapper

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,8 @@ module.exports = {
   testRegex: '\\.test\\.ts$',
   moduleNameMapper: {
     '^utils(.*)$': '<rootDir>/utils$1',
-    '^features/(.*)$': '<rootDir>/features$1',
-    '^api/(.*)$': '<rootDir>/api$1',
+    '^features(.*)$': '<rootDir>/features$1',
+    '^api(.*)$': '<rootDir>/api$1',
   },
   transform: {
     '^.+\\.(t|j)s?$': 'ts-jest',
