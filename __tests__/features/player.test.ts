015618fc948685783c852198efc1a11aa4a573da
SoYoung210 <ethdud1@gmail.com>
2020-01-17 13:34:52 +0900

    Add test case - player reducer

--- __tests__/features/player.test.ts
+++ __tests__/features/player.test.ts
@@ -1,7 +1,7 @@
 import { HttpStatusCode } from 'api'
 import { playerActions, playerReducer, PlayerState } from 'features/player'
 
-describe('[Features - Player]', () => {
+describe('[Features - Player Reducer]', () => {
   const initialState: PlayerState = {
     movie: {
       id: 0,
@@ -14,7 +14,7 @@ describe('[Features - Player]', () => {
     fetchState: HttpStatusCode.LOADING,
   }
 
-  it('', () => {
+  it('play', () => {
     // Given
     const state = initialState
     const actions = {
@@ -27,6 +27,66 @@ describe('[Features - Player]', () => {
     const expected: PlayerState = {
       ...initialState,
       isPlaying: true,
+    }
+
+    expect(result).toEqual(expected)
+  })
+
+  it('pause', () => {
+    // Given
+    const state = initialState
+    const actions = {
+      type: `${playerActions.pause}`,
+    }
+
+    // When
+    const result = playerReducer(state, actions)
+    // Then
+    const expected: PlayerState = {
+      ...initialState,
+      isPlaying: false,
+    }
+
+    expect(result).toEqual(expected)
+  })
+
+  it('updateCurrentTime', () => {
+    // Given
+    const state = initialState
+    const actions = {
+      type: `${playerActions.updateCurrentTime}`,
+      payload: 1,
+    }
+
+    // When
+    const result = playerReducer(state, actions)
+    // Then
+    const expected: PlayerState = {
+      ...initialState,
+      currentTime: 1,
+    }
+
+    expect(result).toEqual(expected)
+  })
+
+  it('fetch', () => {
+    // Given
+    const state = initialState
+    const actions = {
+      type: `${playerActions.fetch}`,
+      payload: 1,
+    }
+
+    // When
+    const result = playerReducer(state, actions)
+    // Then
+    const expected: PlayerState = {
+      ...initialState,
+      movie: {
+        ...initialState.movie,
+        id: 1,
+      },
+      fetchState: HttpStatusCode.LOADING,
     }
 
     expect(result).toEqual(expected)
