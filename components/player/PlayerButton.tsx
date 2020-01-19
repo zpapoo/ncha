7d121ee6de8258351165e672dfaa601bf8cbf411
soso <ethdud1@gmail.com>
2020-01-19 16:43:47 +0900

    Add forward backward button

--- components/player/PlayerButton.tsx
+++ components/player/PlayerButton.tsx
@@ -1,23 +1,44 @@
 import styled from '@emotion/styled'
+import { FlexWrapper } from 'components/common/FlexWrapper'
 import { RootState } from 'features'
 import { playerActions } from 'features/player'
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
+
+// TODO: isPlaying 상황에 따라 url resource가 분기되도록 설정하기.
 const PlayButton = styled.div`
-  margin: auto;
+  margin: 0 10px 0 10px;
   width: 24px;
   height: 24px;
   background-size: contain;
+  align-self: center;
   background-image: url("/images/play.svg");
 `
 
 const PauseButton = styled.div`
-  margin: auto;
+  margin: 0 10px 0 10px;
   width: 24px;
   height: 24px;
   background-size: contain;
+  align-self: center;
   background-image: url("/images/pause.svg");
+`
+
+const ForwardButton = styled.div`
+  width: 30px;
+  height: 30px;
+  background-size: contain;
+  margin-right: auto;
+  background-image: url("/images/forward.svg");
+`
+
+const BackwardButton = styled.div`
+  width: 30px;
+  height: 30px;
+  background-size: contain;
+  margin-left: auto;
+  background-image: url("/images/backward.svg");
 `
 
 export const PlayerButton = () => {
@@ -26,9 +47,15 @@ export const PlayerButton = () => {
     state => state.player.isPlaying,
   )
 
-  if (isPlaying) {
-    return <PauseButton onClick={() => dispatch(playerActions.pause())} />
-  }
-
-  return <PlayButton onClick={() => dispatch(playerActions.play())} />
+  return (
+    <FlexWrapper>
+      <BackwardButton onClick={() => dispatch(playerActions.updateCurrentTime(-5))}/>
+      {
+        isPlaying
+          ? <PauseButton onClick={() => dispatch(playerActions.pause())} />
+          : <PlayButton onClick={() => dispatch(playerActions.play())} />
+      }
+      <ForwardButton onClick={() => dispatch(playerActions.updateCurrentTime(5))}/>
+    </FlexWrapper>
+  )
 }
