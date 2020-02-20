e02f0d0e94648c6068dde8c6b3bb428df2b0cdd4
SoYoung210 <ethdud1@gmail.com>
2020-02-20 13:32:52 +0900

    Need to Fix test case - w.Jbee

--- __tests__/features/playerSaga.test.ts
+++ __tests__/features/playerSaga.test.ts
@@ -1,8 +1,8 @@
 import { FetchStatusCode } from 'api'
 import { RootState } from 'features'
 import { playerToggleSaga } from 'features/playerSaga'
-import { PLAYER_PREFIX, playerActions } from 'features/playerSlice'
-import { select } from 'redux-saga/effects'
+import { PLAYER_PREFIX, playerActions, playerSelectors } from 'features/playerSlice'
+import { delay, select } from 'redux-saga/effects'
 
 const initialState = {
   movie: {
@@ -19,9 +19,6 @@ const initialState = {
 describe('playerToggle saga', () => {
   it('isPlaying값이 true일때 player time을 업데이트 한다.', () => {
     // Given
-    const testAction = {
-      type: `${playerActions.fetch}`,
-    }
     const state = {
       [PLAYER_PREFIX]: {
         ...initialState,
@@ -32,8 +29,9 @@ describe('playerToggle saga', () => {
     // When
 
     // Then
-    expect(gen.next(state as any).value).toEqual(
-      select(({ player }: RootState) => player.isPlaying),
-    )
+    expect(gen.next(state[PLAYER_PREFIX] as any).value).toEqual(select(playerSelectors.isPlaying))
+    expect(gen.next().value).toEqual(delay(1000))
+    // expect(gen.next(state[PLAYER_PREFIX] as any).value).toEqual(select(playerSelectors.times))
+    // expect(gen.next().value).toEqual(playerActions.requestUpdateCurrentTime)
   })
 })
