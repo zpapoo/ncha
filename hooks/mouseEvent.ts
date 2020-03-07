12bfd7cc1ebbcc479bc0740317de59f480101ce1
SoYoung210 <ethdud1@gmail.com>
2020-03-07 15:14:44 +0900

    Change useMouseMoveListener logic

--- hooks/mouseEvent.ts
+++ hooks/mouseEvent.ts
@@ -1,28 +1,40 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useRef, useState } from 'react'
+import { offsetLeft } from 'utils/dom'
 
-interface Props {
-  onMouseMove: (e: TouchEvent | MouseEvent) => void;
-  onMouseUp: (e: TouchEvent | MouseEvent) => void;
-}
+export const useMouseMoveListener = <T extends HTMLElement>() => {
+  const [diff, setDiff] = useState(0)
+  const mouseRef = useRef<T>(null)
 
-export const useMouseMoveListener = ({ onMouseMove, onMouseUp }: Props) => {
-  const [active, setActive] = useState(false)
+  const onMouseMove = useCallback((e: TouchEvent | MouseEvent) => {
+    e.stopPropagation()
 
-  const toggleActive = () => (
-    setActive(!active)
-  )
-
-  useEffect(() => {
-    if (active) {
-      window.addEventListener('mousemove', onMouseMove)
-      window.addEventListener('touchmove', onMouseMove)
-      window.addEventListener('mouseup', onMouseUp)
+    if (e instanceof TouchEvent) {
+      const { clientX } = e.changedTouches[0]
+      const diff = clientX - offsetLeft(mouseRef.current)
+      setDiff(diff)
+    } else if (e instanceof MouseEvent) {
+      requestAnimationFrame(() => {
+        const diff = e.clientX - offsetLeft(mouseRef.current)
+        setDiff(diff)
+      })
     }
+  }, [])
 
+  const onMouseUp = useCallback((e: TouchEvent | MouseEvent) => {
     window.removeEventListener('mousemove', onMouseMove)
     window.removeEventListener('touchmove', onMouseMove)
     window.removeEventListener('mouseup', onMouseUp)
-  }, [active, onMouseMove, onMouseUp])
+  }, [onMouseMove])
 
-  return [toggleActive]
+
+  const onMouseDown = (e: React.MouseEvent | React.TouchEvent) => {
+    e.stopPropagation()
+    // desktop
+    window.addEventListener('mousemove', onMouseMove)
+    // mobile
+    window.addEventListener('touchmove', onMouseMove)
+    window.addEventListener('mouseup', onMouseUp)
+  }
+
+  return { onMouseDown, mouseRef, diff }
 }
