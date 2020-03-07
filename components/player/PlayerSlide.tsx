d40709b0b2f18ced09a1fcf7599f1860fdfc3ff3
SoYoung210 <ethdud1@gmail.com>
2020-03-07 15:14:58 +0900

    Refactor PlayerSlide

--- components/player/PlayerSlide.tsx
+++ components/player/PlayerSlide.tsx
@@ -1,7 +1,8 @@
 import styled from '@emotion/styled'
 import { RootState } from 'features'
 import { playerActions, playerSelectors, PlayerTime } from 'features/playerSlice'
-import React, { useRef } from 'react'
+import { useMouseMoveListener } from 'hooks/mouseEvent'
+import React, { useEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { offsetLeft } from 'utils/dom'
 
@@ -38,66 +39,32 @@ const TimeLine = styled.div`
 
 export const PlayerSlide = () => {
   const dispatch = useDispatch()
-  const sliderRef = useRef<HTMLDivElement>(null)
   const { current, total } = useSelector<RootState, PlayerTime>(playerSelectors.times)
   const width = (current / total) * 100
   const { requestUpdateCurrentTime }= playerActions
 
-  // FIXME: Move할때마다 dispatch하면 너무 성능이 떨어질것 같은데.., TouchEvent도 잡아야 한다.
-  const onMouseUp = (e: TouchEvent | MouseEvent) => {
-    e.stopPropagation()
-    if (e instanceof TouchEvent) {
-
-    } else if (e instanceof MouseEvent) {
-
-    }
-    // TODO: with Custom Hook
-    window.removeEventListener('mousemove', onMouseMove)
-    window.removeEventListener('touchmove', onMouseMove)
-    window.removeEventListener('mouseup', onMouseUp)
-  }
-  const onMouseMove = (e: TouchEvent | MouseEvent) => {
-    e.stopPropagation()
-
-    if (sliderRef.current) {
-      const { offsetWidth } = sliderRef.current
-      if (e instanceof TouchEvent) {
-        const { clientX } = e.changedTouches[0]
-        const diff = clientX - offsetLeft(sliderRef.current)
-        const percent = Math.min(Math.max(diff / offsetWidth, 0), 1)
-        dispatch(requestUpdateCurrentTime(percent * total))
-      } else if (e instanceof MouseEvent) {
-        requestAnimationFrame(() => {
-          const diff = e.clientX - offsetLeft(sliderRef.current)
-          const percent = Math.min(Math.max(diff / offsetWidth, 0), 1)
-          dispatch(requestUpdateCurrentTime(percent * total))
-        })
-      }
-    }
-    // TODO: with Custom Hook
-  }
-
-  const onMouseDown = (e: React.MouseEvent | React.TouchEvent) => {
-    e.stopPropagation()
-    // desktop
-    window.addEventListener('mousemove', onMouseMove)
-    // mobile
-    window.addEventListener('touchmove', onMouseMove)
-    window.addEventListener('mouseup', onMouseUp)
-  }
+  const { onMouseDown, mouseRef, diff } =useMouseMoveListener<HTMLDivElement>()
 
   const onClick = (e: React.MouseEvent) => {
     const { offsetX } = e.nativeEvent
-    if (sliderRef.current) {
-      const { offsetWidth } = sliderRef.current
+    if (mouseRef.current) {
+      const { offsetWidth } = mouseRef.current
       const percent = offsetX / offsetWidth
       dispatch(requestUpdateCurrentTime(percent * total))
     }
   }
 
+  useEffect(() => {
+    if (mouseRef.current) {
+      const { offsetWidth } = mouseRef.current
+      const percent = Math.min(Math.max(diff / offsetWidth, 0), 1)
+      dispatch(requestUpdateCurrentTime(percent * total))
+    }
+  }, [diff, dispatch, mouseRef, requestUpdateCurrentTime, total])
+
   return (
     <TimeLine
-      ref={sliderRef}
+      ref={mouseRef}
       onMouseDown={onMouseDown}
       onTouchStart={onMouseDown}
       onClick={onClick}
