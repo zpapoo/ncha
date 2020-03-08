313514234e3a7b349f408473fbea2699498943bd
SoYoung210 <ethdud1@gmail.com>
2020-03-08 14:41:32 +0900

    Add will-change css for performance

--- components/player/Comments.tsx
+++ components/player/Comments.tsx
@@ -16,6 +16,7 @@ const Wrapper = styled.div`
   padding: 0px 13px 0px 9px;
   font-size: 12px;
   margin-bottom: 17px;
+  will-change: transform;
 `
 
 const ContentWrapper = styled.div`
@@ -34,40 +35,42 @@ const Name = styled.span`
   color: rgba(255, 255, 255, 0.9);
 `
 
-const threshold = 15
 export const Comments = ({ kind, contents, time }: Props) => {
   const [{ x }, set] = useSpring(() => ({ x: 0, y: 0 }))
 
   const bind = useDrag(({ down, movement: [mx, my] }) => {
     set({ x: down ? mx : 0, y: down ? my : 0 })
   }, {
-    bounds: { left: -100, right: 200 },
+    bounds: { left: -150, right: 150 },
   })
 
   return (
-    <animated.div {...bind()} style={
-      {
-        transform: x.interpolate(x => `translateX(${x}px)`),
-      }
-    } >
-      <Wrapper>
-        <ContentWrapper>
-          <ProfileImage />
-          <div>
-            <Name>{COMMENT_KIND[kind]}</Name>
-            {contents.map((content: string, index: number) => {
-              return (
-                <SpeechBubble
-                  key={`${kind}-${index}`}
-                  time={time}
-                  kind={kind}
-                  content={content}
-                />
-              )
-            })}
-          </div>
-        </ContentWrapper>
-      </Wrapper>
-    </animated.div>
+    <>
+      <animated.div {...bind()} style={
+        {
+          transform: x.interpolate(x => `translateX(${x}px)`),
+        }
+      } >
+        <Wrapper>
+          <ContentWrapper>
+            <ProfileImage />
+            <div>
+              <Name>{COMMENT_KIND[kind]}</Name>
+              {contents.map((content: string, index: number) => {
+                return (
+                  <SpeechBubble
+                    key={`${kind}-${index}`}
+                    time={time}
+                    kind={kind}
+                    content={content}
+                  />
+                )
+              })}
+            </div>
+          </ContentWrapper>
+        </Wrapper>
+      </animated.div>
+
+    </>
   )
 }
